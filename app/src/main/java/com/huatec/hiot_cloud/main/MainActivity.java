5d1bcd4e69afc52a0ea74cc4ac07696c208f4ed8
江宇航 <1515936686@qq.com>
2020-04-27 23:11:05 +0800

    2.3：增加主页ui设计，权重，底部栏设计

--- app/src/main/java/com/huatec/hiot_cloud/main/MainActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/main/MainActivity.java
@@ -17,23 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mvc = findViewById(R.id.bt_mvc);
-        Button mvp = findViewById(R.id.bt_mvp);
-
-        mvc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent =new Intent(MainActivity.this, TestMVCActivity.class);
-                startActivity(intent);
-            }
-        });
-        mvp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent  intent =new Intent(MainActivity.this, TestMVPActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
